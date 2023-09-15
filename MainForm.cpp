74ff0dbd62e4006addf38e859783226cc72e7bb8
MKLeo1 <94550363+MKLeo1@users.noreply.github.com>
2023-09-15 03:21:46 +0200

    beta version

---
+++ MainForm.cpp
@@ -0,0 +1,331 @@
+﻿#include "MainForm.hpp"
+#include <windows.system.threading.h>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main() {
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ScannerProject::MainForm form;
+
+	// Full screen
+	form.FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+	form.WindowState = FormWindowState::Maximized;
+
+	Application::Run(% form);
+}
+
+void ScannerProject::MainForm::MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	
+	PictureBox^ backgroundImage = gcnew PictureBox();
+	backgroundImage->Image = Image::FromFile("background.jpg");
+	backgroundImage->SizeMode = PictureBoxSizeMode::Zoom;
+	backgroundImage->Dock = DockStyle::Fill;
+	this->Controls->Add(backgroundImage);
+
+	CodeInputBox->Focus();
+
+	LoadDatabase();
+
+	// !Char::IsDigit
+	CodeInputBox->KeyPress += gcnew KeyPressEventHandler(this, &MainForm::CodeInputBox_KeyPress);
+	CodeInputBox->TextChanged += gcnew EventHandler(this, &MainForm::CodeInputBox_TextChanged);
+	CodeInputBox->LostFocus += gcnew System::EventHandler(this, &MainForm::CodeInputBox_LostFocus);
+	CodeInputBox->GotFocus += gcnew System::EventHandler(this, &MainForm::CodeInputBox_GotFocus);
+	
+	AutoReset->CheckedChanged += gcnew System::EventHandler(this, &MainForm::AutoReset_CheckedChanged);
+	ProductsTable->AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode::Fill;
+	ProductsTable->CellValueChanged += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MainForm::ProductsTable_CellValueChanged);
+	ProductsTable->CellEndEdit += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MainForm::ProductsTable_CellEndEdit);
+	ProductsTable->EditingControlShowing += gcnew DataGridViewEditingControlShowingEventHandler(this, &MainForm::ProductsTable_EditingControlShowing);
+	
+	CurrentProductBox->TextAlign = HorizontalAlignment::Center;
+	
+	WeightSumBox->TextAlign = HorizontalAlignment::Center;
+}
+
+void ScannerProject::MainForm::ProductsTable_EditingControlShowing(System::Object^ sender, System::Windows::Forms::DataGridViewEditingControlShowingEventArgs^ e) {
+	if (ProductsTable->CurrentCell->ColumnIndex == 2)
+	{
+		TextBox^ textBox = dynamic_cast<TextBox^>(e->Control);
+		if (textBox != nullptr)
+		{
+			textBox->KeyPress += gcnew KeyPressEventHandler(this, &MainForm::WeightTextBox_KeyPress);
+		}
+	}
+}
+
+void ScannerProject::MainForm::WeightTextBox_KeyPress(System::Object^ sender, KeyPressEventArgs^ e) {
+	if (!Char::IsDigit(e->KeyChar) && e->KeyChar != ',' && e->KeyChar != 8) {
+		e->Handled = true;
+	}
+}
+
+void ScannerProject::MainForm::ButtonExit_Click(System::Object^ sender, System::EventArgs^ e) {
+	Application::Exit();
+}
+
+void ScannerProject::MainForm::ButtonUndo_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (scannedCodesList->Count > 0) {
+
+		scannedCodesList->RemoveAt(scannedCodesList->Count - 1);
+		productsWeightList->RemoveAt(productsWeightList->Count - 1);
+
+		RefreshDisplay();
+		CodeInputBox->Focus();
+	}
+}
+
+void ScannerProject::MainForm::ButtonReset_Click(System::Object^ sender, System::EventArgs^ e) {
+	ResetForm();
+}
+
+void ScannerProject::MainForm::InputDelayTimer_Elapsed(System::Object^ sender, System::Timers::ElapsedEventArgs^ e) {
+	if (InvokeRequired) {
+		BeginInvoke(gcnew System::Action(this, &ScannerProject::MainForm::AddCodeToContainers));
+	}
+	else {
+		AddCodeToContainers();
+	}
+}
+
+void ScannerProject::MainForm::AutoResetTimer_Elapsed(System::Object^ sender, System::Timers::ElapsedEventArgs^ e) {
+	if (InvokeRequired) {
+		BeginInvoke(gcnew System::Timers::ElapsedEventHandler(this, &MainForm::AutoResetTimer_Elapsed), sender, e);
+		return;
+	}
+
+	if (AutoReset->Checked) {
+		ResetForm();
+		autoResetTimer->Start();
+	}
+}
+
+void ScannerProject::MainForm::ResetForm() {
+	CodeInputBox->Text = nullptr;
+	CurrentProductBox->Text = nullptr;
+	WeightSumBox->Text = nullptr;
+	productsWeightList->Clear();
+	scannedCodesList->Clear();
+	RefreshDisplay();
+	CodeInputBox->Focus();
+}
+
+void ScannerProject::MainForm::AutoReset_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+
+	if (AutoReset->Checked) {
+		autoResetTimer->Start();
+	}
+	else {
+		autoResetTimer->Stop();
+	}
+}
+
+void ScannerProject::MainForm::CodeInputBox_LostFocus(System::Object^ sender, System::EventArgs^ e) {
+	if (!CodeInputBox->Focused) {
+		CodeInputBox->BackColor = Color::Red;
+	}
+}
+
+void ScannerProject::MainForm::CodeInputBox_GotFocus(System::Object^ sender, System::EventArgs^ e) {
+	CodeInputBox->BackColor = SystemColors::Window;
+}
+
+float ScannerProject::MainForm::SumAllWeight() {
+	float sum = 0.0;
+	for each (float weight in productsWeightList) {
+		sum += weight;
+	}
+	return sum;
+} 
+
+void ScannerProject::MainForm::ProductsTable_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+
+}
+
+void ScannerProject::MainForm::ProductsTable_CellEndEdit(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	if (e->RowIndex >= 0 && e->ColumnIndex == 2) { 
+
+		String^ newValue = ProductsTable->Rows[e->RowIndex]->Cells[e->ColumnIndex]->Value->ToString();
+		float newWeight;
+
+		if (float::TryParse(newValue, newWeight)) {
+			if (e->RowIndex < productsWeightList->Count) {
+			
+				productsWeightList[e->RowIndex] = newWeight;
+				RefreshDisplay();
+			}
+		}
+	}
+	CodeInputBox->Focus();
+}
+
+void ScannerProject::MainForm::ProductsTable_CellValueChanged(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+
+void ScannerProject::MainForm::UpdateProductsTable() {
+	ProductsTable->Rows->Clear();
+	ProductsTable->Columns->Clear();
+
+	DataGridViewTextBoxColumn^ indexColumn = gcnew DataGridViewTextBoxColumn();
+	indexColumn->AutoSizeMode = DataGridViewAutoSizeColumnMode::None;
+	indexColumn->HeaderText = "";
+	indexColumn->Width = 30;
+	indexColumn->ReadOnly = true;
+	ProductsTable->Columns->Add(indexColumn);
+
+	DataGridViewTextBoxColumn^ codeColumn = gcnew DataGridViewTextBoxColumn();
+	codeColumn->HeaderText = "Code";
+	codeColumn->Width = 100;
+	codeColumn->ReadOnly = true;
+	ProductsTable->Columns->Add(codeColumn);
+
+	DataGridViewTextBoxColumn^ weightColumn = gcnew DataGridViewTextBoxColumn();
+	weightColumn->HeaderText = "Weight (kg)";
+	weightColumn->Width = 100;
+	weightColumn->ReadOnly = false;
+	ProductsTable->Columns->Add(weightColumn);
+
+	DataGridViewTextBoxColumn^ nameColumn = gcnew DataGridViewTextBoxColumn();
+	nameColumn->HeaderText = "Product Name";
+	nameColumn->Width = 200;
+	nameColumn->ReadOnly = true;
+	ProductsTable->Columns->Add(nameColumn);
+
+	int currentIndex = 1;
+
+	for (int i = 0; i < scannedCodesList->Count; ++i) {
+		
+		String^ scannedCode = scannedCodesList[i];
+		String^ displayedWeight = FormatWeight(productsWeightList[i]);
+		String^ recognizedProductName = RecognizeCode(scannedCode);
+
+		ProductsTable->Rows->Add(currentIndex, scannedCode, displayedWeight, recognizedProductName);
+
+		++currentIndex;
+	}
+
+	if (ProductsTable->RowCount > 0) {
+		ProductsTable->FirstDisplayedScrollingRowIndex = ProductsTable->RowCount - 1;
+	}
+}
+
+String^ ScannerProject::MainForm::FormatWeight(float weight) {
+	String^ formattedWeight;
+	if (weight == (int)weight) {
+		formattedWeight = String::Format("{0:0.00}", weight);
+	}
+	else {
+		formattedWeight = String::Format("{0:0.###}", weight);
+	}
+	return formattedWeight;
+}
+
+void ScannerProject::MainForm::SetFocusToCodeInputBox() {
+	CodeInputBox->Focus();
+}
+
+void ScannerProject::MainForm::RefreshDisplay() {
+	float totalWeight = SumAllWeight();
+	String^ formattedWeight = FormatWeight(totalWeight);
+	WeightSumBox->Text = formattedWeight + " kg";
+	WeightSumBox->Refresh();
+	UpdateProductsTable();
+}
+
+void ScannerProject::MainForm::WeightSumBox_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+}
+
+void ScannerProject::MainForm::CodeInputBox_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	inputDelayTimer->Stop();
+	inputDelayTimer->Start();
+}
+
+void ScannerProject::MainForm::AddCodeToContainers() {
+	String^ scannedCode = CodeInputBox->Text;
+
+	String^ recognizedProduct = RecognizeCode(scannedCode);
+	if (!String::IsNullOrEmpty(recognizedProduct)) {
+		CurrentProductBox->Clear();
+		CurrentProductBox->Text = recognizedProduct;
+
+		float extractedWeight = ExtractWeight(scannedCode);
+
+		scannedCodesList->Add(scannedCode);
+		productsWeightList->Add(extractedWeight);
+
+		RefreshDisplay();
+
+		CodeInputBox->Clear();
+	}
+	else {
+		if (!String::IsNullOrEmpty(scannedCode)) {
+			CurrentProductBox->Text = "Unrecognized product";
+		}
+		CodeInputBox->Clear();
+	}
+}
+
+void ScannerProject::MainForm::CodeInputBox_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+	if (!Char::IsDigit(e->KeyChar)) {
+		e->Handled = true;
+	}
+}
+
+float ScannerProject::MainForm::ExtractWeight(String^ scannedCode) {
+	int codeLength = scannedCode->Length;
+	String^ lastFourDigits = scannedCode->Substring(codeLength - 5, 4);
+
+	if (codeLength == 13) {
+		return float::Parse(lastFourDigits) / 1000.0f;
+	}
+	if (codeLength == 20) {
+		return float::Parse(lastFourDigits) / 100.0f;
+	}
+	else return 0.000f;
+}
+
+String^ ScannerProject::MainForm::RecognizeCode(String^ scannedCode) {
+
+	String^ recognizedProduct = "";
+	String^ subScannedCode = "";
+
+	if (scannedCode->Length == 13) {
+		subScannedCode = scannedCode->Substring(0, 7);
+	}
+	if (scannedCode->Length == 20) {
+		subScannedCode = scannedCode->Substring(0, 14);
+	}
+	if (databaseDictionary->ContainsKey(subScannedCode)) {
+		recognizedProduct = databaseDictionary[subScannedCode];
+	}
+	return recognizedProduct;
+}
+
+void ScannerProject::MainForm::LoadDatabase() {
+	try {
+		System::IO::StreamReader^ inputFile = gcnew System::IO::StreamReader("Insert/here/path/to/database");
+		if (!inputFile) {
+			MessageBox::Show("Database not found", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			return;
+		}
+
+		while (!inputFile->EndOfStream) {
+			String^ line = inputFile->ReadLine();
+			array<String^>^ parts = line->Split('|');
+
+			if (parts->Length == 2) {
+				String^ databaseCode = parts[0];
+				String^ productName = parts[1];
+				databaseDictionary->Add(databaseCode, productName);
+			}
+		}
+		inputFile->Close();
+	}
+	catch (Exception^ e) {
+		MessageBox::Show("Database error: " + e->Message, "Błąd", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+}
---
+++ MainForm.hpp
@@ -0,0 +1,235 @@
+#pragma once
+
+namespace ScannerProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Collections::Generic;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Timers;
+	using namespace System::Threading;
+
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			scannedCodesList = gcnew List<String^>();
+			productsWeightList = gcnew List<float>();
+			databaseDictionary = gcnew Dictionary<String^, String^>();
+
+			inputDelayTimer = gcnew System::Timers::Timer();
+			inputDelayTimer->Interval = 500;
+			inputDelayTimer->Elapsed += gcnew System::Timers::ElapsedEventHandler(this, &MainForm::InputDelayTimer_Elapsed);
+			
+			autoResetTimer = gcnew System::Timers::Timer();
+			autoResetTimer->Interval = 100000;
+			autoResetTimer->AutoReset = false;
+			autoResetTimer->Elapsed += gcnew System::Timers::ElapsedEventHandler(this, &MainForm::AutoResetTimer_Elapsed);
+		}
+	protected:
+
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private:
+
+    List<String^>^ scannedCodesList;
+	List<float>^ productsWeightList;
+    Dictionary<String^, String^>^ databaseDictionary;
+
+	System::Void MainForm_Load(System::Object^, System::EventArgs^);
+	System::Void ButtonExit_Click(System::Object^, System::EventArgs^);
+	System::Void ButtonUndo_Click(System::Object^, System::EventArgs^);
+	System::Void ButtonReset_Click(System::Object^, System::EventArgs^);
+	System::Void WeightSumBox_TextChanged(System::Object^, System::EventArgs^);
+	System::Void CodeInputBox_TextChanged(System::Object^, System::EventArgs^);
+	System::Void CodeInputBox_KeyPress(System::Object^, System::Windows::Forms::KeyPressEventArgs^);
+	System::Void ProductsTable_CellContentClick(System::Object^, System::Windows::Forms::DataGridViewCellEventArgs^);
+	System::Void ProductsTable_CellEndEdit(System::Object^, System::Windows::Forms::DataGridViewCellEventArgs^);
+	System::Void ProductsTable_CellValueChanged(System::Object^, System::Windows::Forms::DataGridViewCellEventArgs^);
+	System::Void ProductsTable_EditingControlShowing(System::Object^, System::Windows::Forms::DataGridViewEditingControlShowingEventArgs^);
+	System::Void WeightTextBox_KeyPress(System::Object^, System::Windows::Forms::KeyPressEventArgs^);
+	
+	System::Void MainForm::InputDelayTimer_Elapsed(System::Object^, System::Timers::ElapsedEventArgs^);
+	System::Void MainForm::AutoResetTimer_Elapsed(System::Object^, System::Timers::ElapsedEventArgs^);
+	System::Void MainForm::AutoReset_CheckedChanged(System::Object^, System::EventArgs^);
+	System::Void MainForm::CodeInputBox_LostFocus(System::Object^ sender, System::EventArgs^ e);
+	System::Void MainForm::CodeInputBox_GotFocus(System::Object^ sender, System::EventArgs^ e);
+
+	System::ComponentModel::Container ^components;
+
+	float SumAllWeight();
+	float ExtractWeight(String^);
+	void AddCodeToContainers();
+	void ResetForm();
+	void RefreshDisplay();
+	void UpdateProductsTable();
+	void LoadDatabase();
+	void SetFocusToCodeInputBox();
+	String^ RecognizeCode(String^);
+	String^ FormatWeight(float);
+
+	System::Timers::Timer^ autoResetTimer;
+	System::Timers::Timer^ inputDelayTimer;
+
+	System::Windows::Forms::DataGridView^ ProductsTable;
+	System::Windows::Forms::Button^ ButtonReset;
+	System::Windows::Forms::Button^ ButtonExit;
+    System::Windows::Forms::Button^ ButtonUndo;
+	System::Windows::Forms::CheckBox^ AutoReset;
+	System::Windows::Forms::TextBox^ CurrentProductBox;	
+	System::Windows::Forms::TextBox^ WeightSumBox;
+	System::Windows::Forms::TextBox^ CodeInputBox;
+
+#pragma region Windows Form Designer generated code
+
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			this->CurrentProductBox = (gcnew System::Windows::Forms::TextBox());
+			this->ProductsTable = (gcnew System::Windows::Forms::DataGridView());
+			this->ButtonReset = (gcnew System::Windows::Forms::Button());
+			this->ButtonExit = (gcnew System::Windows::Forms::Button());
+			this->ButtonUndo = (gcnew System::Windows::Forms::Button());
+			this->AutoReset = (gcnew System::Windows::Forms::CheckBox());
+			this->WeightSumBox = (gcnew System::Windows::Forms::TextBox());
+			this->CodeInputBox = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ProductsTable))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// CurrentProductBox
+			// 
+			this->CurrentProductBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->CurrentProductBox->Location = System::Drawing::Point(24, 12);
+			this->CurrentProductBox->Multiline = true;
+			this->CurrentProductBox->Name = L"CurrentProductBox";
+			this->CurrentProductBox->Size = System::Drawing::Size(1365, 77);
+			this->CurrentProductBox->TabIndex = 0;
+			// 
+			// ProductsTable
+			// 
+			this->ProductsTable->BackgroundColor = System::Drawing::SystemColors::Window;
+			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle2->BackColor = System::Drawing::SystemColors::Control;
+			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			dataGridViewCellStyle2->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->ProductsTable->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			this->ProductsTable->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->ProductsTable->Location = System::Drawing::Point(387, 133);
+			this->ProductsTable->Name = L"ProductsTable";
+			this->ProductsTable->RowHeadersWidth = 62;
+			this->ProductsTable->Size = System::Drawing::Size(642, 633);
+			this->ProductsTable->TabIndex = 1;
+			this->ProductsTable->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MainForm::ProductsTable_CellContentClick);
+			this->ProductsTable->CellValueChanged += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MainForm::ProductsTable_CellValueChanged);
+			// 
+			// ButtonReset
+			// 
+			this->ButtonReset->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->ButtonReset->Location = System::Drawing::Point(1089, 358);
+			this->ButtonReset->Name = L"ButtonReset";
+			this->ButtonReset->Size = System::Drawing::Size(300, 151);
+			this->ButtonReset->TabIndex = 2;
+			this->ButtonReset->Text = L"Reset";
+			this->ButtonReset->UseVisualStyleBackColor = true;
+			this->ButtonReset->Click += gcnew System::EventHandler(this, &MainForm::ButtonReset_Click);
+			// 
+			// ButtonExit
+			// 
+			this->ButtonExit->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->ButtonExit->Location = System::Drawing::Point(1089, 637);
+			this->ButtonExit->Name = L"ButtonExit";
+			this->ButtonExit->Size = System::Drawing::Size(300, 129);
+			this->ButtonExit->TabIndex = 3;
+			this->ButtonExit->Text = L"Wy³¹cz aplikacje";
+			this->ButtonExit->UseVisualStyleBackColor = true;
+			this->ButtonExit->Click += gcnew System::EventHandler(this, &MainForm::ButtonExit_Click);
+			// 
+			// ButtonUndo
+			// 
+			this->ButtonUndo->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->ButtonUndo->Location = System::Drawing::Point(1089, 133);
+			this->ButtonUndo->Name = L"ButtonUndo";
+			this->ButtonUndo->Size = System::Drawing::Size(300, 151);
+			this->ButtonUndo->TabIndex = 4;
+			this->ButtonUndo->Text = L"Cofnij";
+			this->ButtonUndo->UseVisualStyleBackColor = true;
+			this->ButtonUndo->Click += gcnew System::EventHandler(this, &MainForm::ButtonUndo_Click);
+			// 
+			// AutoReset
+			// 
+			this->AutoReset->AutoSize = true;
+			this->AutoReset->BackColor = System::Drawing::Color::LightGray;
+			this->AutoReset->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->AutoReset->Location = System::Drawing::Point(1089, 553);
+			this->AutoReset->Name = L"AutoReset";
+			this->AutoReset->Size = System::Drawing::Size(184, 40);
+			this->AutoReset->TabIndex = 5;
+			this->AutoReset->Text = L"Auto Reset";
+			this->AutoReset->UseVisualStyleBackColor = false;
+			this->AutoReset->CheckedChanged += gcnew System::EventHandler(this, &MainForm::AutoReset_CheckedChanged);
+			// 
+			// WeightSumBox
+			// 
+			this->WeightSumBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->WeightSumBox->Location = System::Drawing::Point(24, 637);
+			this->WeightSumBox->Multiline = true;
+			this->WeightSumBox->Name = L"WeightSumBox";
+			this->WeightSumBox->Size = System::Drawing::Size(300, 129);
+			this->WeightSumBox->TabIndex = 6;
+			this->WeightSumBox->TextChanged += gcnew System::EventHandler(this, &MainForm::WeightSumBox_TextChanged);
+			// 
+			// CodeInputBox
+			// 
+			this->CodeInputBox->Location = System::Drawing::Point(387, 95);
+			this->CodeInputBox->Multiline = true;
+			this->CodeInputBox->Name = L"CodeInputBox";
+			this->CodeInputBox->Size = System::Drawing::Size(642, 32);
+			this->CodeInputBox->TabIndex = 7;
+			this->CodeInputBox->TextChanged += gcnew System::EventHandler(this, &MainForm::CodeInputBox_TextChanged);
+			this->CodeInputBox->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &MainForm::CodeInputBox_KeyPress);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Black;
+			this->ClientSize = System::Drawing::Size(1415, 778);
+			this->Controls->Add(this->CodeInputBox);
+			this->Controls->Add(this->WeightSumBox);
+			this->Controls->Add(this->AutoReset);
+			this->Controls->Add(this->ButtonUndo);
+			this->Controls->Add(this->ButtonExit);
+			this->Controls->Add(this->ButtonReset);
+			this->Controls->Add(this->ProductsTable);
+			this->Controls->Add(this->CurrentProductBox);
+			this->Name = L"MainForm";
+			this->Text = L"Scanner";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ProductsTable))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+};
+}
